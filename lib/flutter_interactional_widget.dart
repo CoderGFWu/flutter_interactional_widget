9ff74e53de10c219c56ad2ccb5c62f66441f4f5f
CoderGFWu <1561884043@qq.com>
2023-03-06 14:39:35 +0800

    Update flutter_interactional_widget.dart

--- lib/flutter_interactional_widget.dart
+++ lib/flutter_interactional_widget.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sensors_plus/sensors_plus.dart';
+import 'package:sensors/sensors.dart';
 
 // ignore: must_be_immutable
 class InteractionalWidget extends StatefulWidget {
